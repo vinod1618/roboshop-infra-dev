e06f510033e75852e0132b8cecefe7a067db5a24
Vinod <vinodsv1618@gmail.com>
2026-05-11 18:44:02 +0530

    hello

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -5,6 +5,7 @@ component=$1
 cd /home/ec2-user
 git clone https://github.com/vinod1618/ansible-roboshop-roles-tf.git
 
-git pull
+
 cd ansible-roboshop-roles-tf 
+git pull
 ansible-playbook -e component=$component roboshop.yaml
