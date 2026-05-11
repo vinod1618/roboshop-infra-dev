14a4154604997f953e1434f1ea602c1f2bceb72b
Vinod <vinodsv1618@gmail.com>
2026-05-11 19:17:46 +0530

    hello

--- 40-databases/local.tf
+++ 40-databases/local.tf
@@ -4,6 +4,7 @@ locals {
     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
     redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
     mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
+    my_sql_role_name = join ("-", [for name in ["${var.project}","${var.environment}","mysql"]: title(name)] )
 
     common_tags = {
         Project = var.project
---
+++ 40-databases/mysql_IAM.tf
@@ -0,0 +1,26 @@
+resource "aws_iam_role" "mysql" {
+  name = local.my_sql_role_name
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = merge (
+    {
+        Name = local.my_sql_role_name
+    },
+    local.common_tags
+  )
+}
