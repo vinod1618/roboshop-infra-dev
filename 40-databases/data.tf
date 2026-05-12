6a8dd60ed34d6f7916a9e4788bc28c423dbc7552
Vinod <vinodsv1618@gmail.com>
2026-05-12 17:52:10 +0530

    hello

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -34,3 +34,7 @@ data "aws_ssm_parameter" "redis_sg_id" {
 data "aws_ssm_parameter" "mysql_sg_id" {
   name  = "/${var.project}/${var.environment}/mysql_sg_id"
 }
+
+data "aws_ssm_parameter" "rabbitmq_sg_id" {
+  name  = "/${var.project}/${var.environment}/rabbitmq_sg_id"
+}
