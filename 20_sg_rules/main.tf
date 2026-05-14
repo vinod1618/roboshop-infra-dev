a14ed055ef1d915c97465f99de4cb44aa089f711
Vinod <vinodsv1618@gmail.com>
2026-05-14 15:59:34 +0530

    hello

--- 20_sg_rules/main.tf
+++ 20_sg_rules/main.tf
@@ -84,3 +84,24 @@ resource "aws_security_group_rule" "backend_alb_bastion" {
   source_security_group_id = local.bastion_sg_id
   security_group_id = local.backend_alb_sg_id
 }
+
+# catalogue
+
+resource "aws_security_group_rule" "catalogue_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.catalogue_sg_id
+}
+
+
+resource "aws_security_group_rule" "catalogue_backend_alb" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  source_security_group_id = local.backend_alb_sg_id
+  security_group_id = local.catalogue_sg_id
+}
