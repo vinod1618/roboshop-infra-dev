b0ef7a29a99054d7dcac570ffb7e245527a8fc59
Vinod <vinodsv1618@gmail.com>
2026-05-15 15:37:02 +0530

    hello

--- 50-backend-alb/data.tf
+++ 50-backend-alb/data.tf
@@ -4,5 +4,5 @@ data "aws_ssm_parameter" "backend_alb_sg_id" {
 
 
 data "aws_ssm_parameter" "private_subnet_ids" {
-  name  = "/${var.project}/${var.environment}/private_subnet_ids"
+  name  = "/${var.project}/${var.environment}/private_subnet_id"
 }
