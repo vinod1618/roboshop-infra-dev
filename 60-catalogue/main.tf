9a0de9d0d572c10b6228e4b9de2d823e5c507afc
Vinod <vinodsv1618@gmail.com>
2026-05-15 16:44:12 +0530

    hello

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -44,4 +44,19 @@ resource "terraform_data" "catalogue" {
 resource "aws_ec2_instance_state" "catalogue" {
   instance_id = aws_instance.catalogue.id
   state       = "stopped" # Change to "running" to start it back up
+  depends_on = [terraform_data.catalogue]
+}
+
+
+resource "aws_ami_from_instance" "catalogue" {
+  name               = "${var.project}-${var.environment}-catalogue"
+  source_instance_id = aws_instance.catalogue.id
+  depends_on = [aws_ec2_instance_state.catalogue] 
+  tags = merge (
+    {
+        Name = "${var.project}-${var.environment}-catalogue"
+    },
+    local.common_tags
+
+  )
 }
