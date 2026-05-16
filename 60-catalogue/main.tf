afab4208b57324b7442a2f364a8d797eb59f6029
Vinod <vinodsv1618@gmail.com>
2026-05-16 22:33:05 +0530

    hello

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -179,13 +179,14 @@ resource "aws_autoscaling_group" "catalogue" {
 resource "aws_autoscaling_policy" "catalogue" {
   autoscaling_group_name = aws_autoscaling_group.catalogue.name
   policy_type            = "TargetTrackingScaling"
-
+  estimated_instance_warmup = 120
   target_tracking_configuration {
     predefined_metric_specification {
       predefined_metric_type = "ASGAverageCPUUtilization"
     }
 
     target_value = 70.0
+
   }
 
 
