8d7e77e8cca878e2d4114445569b47caca230873
Vinod <vinodsv1618@gmail.com>
2026-05-16 15:10:30 +0530

    hello

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -148,7 +148,7 @@ resource "aws_autoscaling_group" "catalogue" {
     id      = aws_launch_template.catalogue.id
     version = "$Latest"
   }
-  vpc_zone_identifier       = [local.private_subnet-id.id]
+  vpc_zone_identifier       = [local.private_subnet_id.id]
 
   tag {
     key                 = "Name"
