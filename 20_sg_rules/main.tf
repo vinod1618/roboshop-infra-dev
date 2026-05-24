b63db134fa176018df1464f498b7500b1ff59d2c
Vinod <vinodsv1618@gmail.com>
2026-05-24 16:29:32 +0530

    hello

--- 20_sg_rules/main.tf
+++ 20_sg_rules/main.tf
@@ -304,6 +304,15 @@ resource "aws_security_group_rule" "frontend_frontend_alb" {
   security_group_id = local.frontend_sg_id
 }
 
+resource "aws_security_group_rule" "frontend_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.frontend_sg_id
+}
+
 
 #frontendalb
 
