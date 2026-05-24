6ba8e9ea37d5edd9869585da4c0b08a1c14405a6
Vinod <vinodsv1618@gmail.com>
2026-05-24 12:32:09 +0530

    hello

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -36,7 +36,7 @@ resource "terraform_data" "mongodb" {
     provisioner "remote-exec" {
     inline = [
         "chmod +x /tmp/bootstrap.sh",
-        "sudo sh /tmp/bootstrap.sh mongodb"
+        "sudo sh /tmp/bootstrap.sh mongodb ${var.environment}"
     ]
   }
 }
@@ -81,7 +81,7 @@ resource "terraform_data" "redis" {
     provisioner "remote-exec" {
     inline = [
         "chmod +x /tmp/bootstrap.sh",
-        "sudo sh /tmp/bootstrap.sh redis"
+        "sudo sh /tmp/bootstrap.sh redis ${var.environment}"
     ]
   }
 }
@@ -168,8 +168,7 @@ resource "terraform_data" "rabbitmq" {
     provisioner "remote-exec" {
     inline = [
         "chmod +x /tmp/bootstrap.sh",
-        "sudo sh /tmp/bootstrap.sh rabbitmq dev"
+        "sudo sh /tmp/bootstrap.sh rabbitmq ${var.environment}"
     ]
   }
 }
-
