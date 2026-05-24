135118230aaab19960b963fdf263d0e6636ac8f5
Vinod <vinodsv1618@gmail.com>
2026-05-24 15:15:06 +0530

    hello

---
+++ 90-components/variables.tf
@@ -0,0 +1,28 @@
+variable "component"{
+    default = {
+        catalogue = {
+            rule_priority = 10
+        }
+
+        user = {
+            rule_priority = 20
+        }
+
+        cart = {
+            rule_priority = 30
+        }
+
+        shipping = {
+            rule_priority = 40
+        }
+
+        payment = {
+            rule_priority = 50
+        }
+        # this is attaching to frontend alb
+        frontend = {
+            rule_priority = 10
+        }
+        
+    }
+}
