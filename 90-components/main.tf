6fb8db4e649227a9fa744ca997761b9dee91cbf9
Vinod <vinodsv1618@gmail.com>
2026-05-24 15:21:47 +0530

    hello

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,6 @@
 module "component"{
     for_each = var.component
-    source = "git:https://github.com/vinod1618/terraform-roboshop-component.git?ref=main"
+    source = "git::https://github.com/vinod1618/terraform-roboshop-component.git?ref=main"
     component = each.key
     rule_priority = each.value.rule_priority
 }
